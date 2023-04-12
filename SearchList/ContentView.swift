2133149c7c62d30eb4ea30da7a020bd056a56b2c
Calum Couch <ccouch@lcs.on.ca>
2023-04-12 12:06:42 -0400

    added search feature

--- SearchList/ContentView.swift
+++ SearchList/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 struct ContentView: View {
     // MARK: Stored properties
     @State var items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+    
+    // Search term the user has provided
+    @State var searchText = ""
+    
+    
     // MARK: Computed properties
     var body: some View {
-        List(items, id: \.self) { currentItem in
-            Text(currentItem)
-        }
+        
+        NavigationView {
+        VStack {
+            
+        Text("Searching on: \(searchText)")
+            
+            List(items, id: \.self){ currentItem in
+                Text (currentItem)
+            }
+        .searchable (text: $searchText)
+                     }
+            }
     }
 }
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+                     struct ContentView_Previews: PreviewProvider {
+                    static var previews: some View {
+                        ContentView()
+                    }
+                }
